bbd52c47ffc9da836f0e5c8827cfbe37fce724f9
Torres2235 <Torres2235@gmail.com>
2024-02-24 15:33:14 -0800

    revamped contact.js

--- src/contact.js
+++ src/contact.js
@@ -1,43 +1,68 @@
+class Item {
+    constructor(name, title, phoneNum, email) {
+        this.name = name;
+        this.title = title;
+        this.phoneNum = phoneNum;
+        this.email = email;
+    }
+};
+
+const contactItems = [
+    new Item(
+        "Monica Geller",
+        "Head Chef",
+        "1-123-4567",
+        "monicaGeller@email.com"
+    ),
+    new Item(
+        "Gordan Ramsey",
+        "Owner",
+        "1-222-3333",
+        "gordanRamsey@email.com"
+    ),
+    new Item(
+        "Michael Scott",
+        "Manager",
+        "1-999-888",
+        "michealScott@email.com"
+    )
+];
+
 export default function contact(content) {
+    const contactContainer = document.createElement('div');
+    contactContainer.classList.add('contactContainer');
+    const contactSection = document.createElement("h1");
+    contactSection.textContent = "Contact Us";
+    contactContainer.appendChild(contactSection);
 
-    const heading = document.createElement("h1");
-    heading.innerText = "Contact Us";
-    content.appendChild(heading);
+    contactItems.forEach(item => {
+        const contactItem = document.createElement('div');
+        contactItem.classList.add("contactItem");
 
-    const sec1 = document.createElement("div");
-    const contact1 = document.createElement("h2");
-    contact1.innerText = "Owner Name";
-    const info1 = document.createElement("div")
-    const title = document.createElement("div");
-    title.innerText = "Owner";
-    const num = document.createElement("div");
-    num.innerText = "1-234-5678";
-    const email = document.createElement("div");
-    email.innerText = "realEmail@website.com";
-    sec1.appendChild(contact1);
-    info1.appendChild(title);
-    info1.appendChild(num);
-    info1.appendChild(email);
-    sec1.appendChild(info1);
+        const contactName = document.createElement("h2");
+        contactName.classList.add("contactName");
+        contactName.innerText = item.name;
 
+        const contactTitle = document.createElement('div');
+        contactTitle.classList.add("contactTitle");
+        contactTitle.innerText = item.title;
 
-    const sec2 = document.createElement("div");
-    const contact2 = document.createElement("h2");
-    contact2.innerText = "Chef Name";
-    const info2 = document.createElement("div")
-    const t = document.createElement("div");
-    t.innerText = "Chef";
-    const n = document.createElement("div");
-    n.innerText = "5-555-5555";
-    const e = document.createElement("div");
-    e.innerText = "anotherRealEmail@website.com";
-    sec2.appendChild(contact2);
-    info2.appendChild(t);
-    info2.appendChild(n);
-    info2.appendChild(e);
-    sec2.appendChild(info2);
+        const contactNum = document.createElement('div');
+        contactNum.classList.add("contactNum");
+        contactNum.innerText = item.phoneNum;
+
+        const contactEmail = document.createElement('div');
+        contactEmail.classList.add("contactEmail");
+        contactEmail.innerText = item.email;
+
+        contactItem.appendChild(contactName);
+        contactItem.appendChild(contactTitle);
+        contactItem.appendChild(contactNum);
+        contactItem.appendChild(contactEmail);
+
+        contactContainer.appendChild(contactItem);
+    });
 
 
-    content.appendChild(sec1);
-    content.appendChild(sec2);
+    content.appendChild(contactContainer);
 };
